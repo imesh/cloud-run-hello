c9e46fa1725274660281a7d0ff85d0fc791280fb
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2020-07-09 20:01:20 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('Request received: [url] ' + req.url);
-    res.end('Hello world from Cloud Run!');
+    res.end('Hello world from Cloud Run + CI/CD!');
 });
 
 const PORT = process.env.PORT || 8080;
